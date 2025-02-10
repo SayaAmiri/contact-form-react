622d71f662590d92c91119831b921daf4303169b
SayaAmiri <sayaamiri00@gmail.com>
2025-02-10 15:49:34 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ContactForm from "./components/ContactForm";
+
+export default function App() {
+  return (
+    <div style={{background:"hsl(148, 38%, 91%)"}}>
+        <ContactForm />
+    </div>
+  )
+}
